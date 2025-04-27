29b4760b5aa91fd062b6f8ae9011d70006a81779
StenioEric <stenioeric@gmail.com>
2025-04-27 15:07:36 -0300

    adicionada exclusão de compra

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,14 @@
 const form = document.querySelector("form");
 const product = document.getElementById("product");
-const itemList = document.querySelector(".item-list"); // Pega a UL
+const itemList = document.querySelector(".item-list");
+const btn = document.querySelector(".btn-delete");
+const item = document.querySelectorAll(".item");
+const error = document.querySelector("#error");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  let itemValue = product.value // Captura o valor do input
+  let itemValue = product.value; // Captura o valor do input
   adiciona(itemValue);
   product.value = ""; // Limpa o input depois de adicionar
 });
@@ -18,11 +21,28 @@ function adiciona(productName) {
   li.innerHTML = `
     <div class="flex gap-1 margin-left-12">
       <input type="checkbox" />
-      <p class="item">${productName}</p>
+      <p>${productName}</p>
     </div>
-    <img class="trash" src="assets/icons/trash.svg" alt="ícone de lixeira" />
-  `;
 
+    <button onclick="deletCompra(this)" class="btn-delete">
+      <img class="trash" src="assets/icons/trash.svg" alt="ícone de lixeira" />
+    </button>
+  `;
   // Adiciona o novo li dentro da UL
   itemList.appendChild(li);
 }
+
+
+function deletCompra(li) {
+  // O this remete ao própio elemento que chamou a função
+  // O parentElement é o elemento pai do button, ou seja, a li
+  // O parentElement do button é a li, então removemos a li
+  li.parentElement.remove();
+  error.classList.remove("close"); // Adiciona a classe 'close' no erro (esconde o erro)
+  // Adiciona a classe 'close' no erro depois de 2 segundos
+  setTimeout(() => {
+    error.classList.add("close"); // Remove a classe 'close' do erro (mostra o erro)
+  }, 2000);
+}
+
+
