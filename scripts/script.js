ca2d6fd0ad23b719d38a01444babc2c0e3592104
StenioEric <stenioeric@gmail.com>
2025-04-27 21:00:04 -0300

    adicionada função de ocultar elemento

--- scripts/script.js
+++ scripts/script.js
@@ -9,11 +9,11 @@ form.addEventListener("submit", (e) => {
   e.preventDefault();
 
   let itemValue = product.value; // Captura o valor do input
-  adiciona(itemValue);
+  newItem(itemValue);
   product.value = ""; // Limpa o input depois de adicionar
 });
 
-function adiciona(productName) {
+function newItem(productName) {
   // Cria a estrutura de um novo item da lista
   const li = document.createElement("li");
   li.className = "item flex";
@@ -24,25 +24,24 @@ function adiciona(productName) {
       <p>${productName}</p>
     </div>
 
-    <button onclick="deletCompra(this)" class="btn-delete">
-      <img class="trash" src="assets/icons/trash.svg" alt="ícone de lixeira" />
+    <button onclick="deletItem(this)" class="btn-close">
+      <img class="icon-trash" src="assets/icons/trash.svg" alt="ícone de lixeira" />
     </button>
   `;
   // Adiciona o novo li dentro da UL
   itemList.appendChild(li);
 }
 
-
-function deletCompra(li) {
+function deletItem(btn) {
   // O this remete ao própio elemento que chamou a função
-  // O parentElement é o elemento pai do button, ou seja, a li
-  // O parentElement do button é a li, então removemos a li
-  li.parentElement.remove();
+  // O parentElement é o elemento pai do button
+  btn.parentElement.remove();
   error.classList.remove("close"); // Adiciona a classe 'close' no erro (esconde o erro)
+
   // Adiciona a classe 'close' no erro depois de 2 segundos
   setTimeout(() => {
-    error.classList.add("close"); // Remove a classe 'close' do erro (mostra o erro)
-  }, 2000);
+    error.classList.add("close"); // Adiciona a classe 'close' do erro 
+  }, 3000);
 }
 
 
